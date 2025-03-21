98ac325813325eafc514588ba09c721c299cd6df
Hoàng Khắc Tiến <161594088+TienCE04@users.noreply.github.com>
2025-03-21 17:41:37 +0700

    cập nhật file

--- bai1.cpp
+++ bai1.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
-    cout<<"hoang khac tien"<<endl;
+    cout<<"hoang khac tien 2004"<<endl;
     cout<<"hello"<<endl;
+    int n;
+    cin>n;
+    int s=0;
+    for(int i=1;i<=10;i++){
+        s+=i;
+    }
+    cout<<s<<endl;
 }
